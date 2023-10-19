51e7eb9f21fa39c105c6b64513b3f2e02e577400
Michael Dennis <michaeljdennis@gmail.com>
2023-10-19 14:18:52 -0700

    Add gopls settings

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -10,4 +10,13 @@ lspconfig.gopls.setup {
 	cmd = {"gopls"},
 	filetypes = {"go", "gomod", "gowork", "gotmpl"},
 	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+			},
+		},
+	},
 }
